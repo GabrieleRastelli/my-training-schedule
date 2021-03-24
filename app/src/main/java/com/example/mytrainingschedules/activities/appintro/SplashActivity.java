61f97a80a964819d23c217d281d61fc80e5dfca1
mattiagualtieri <mattiagualtieri2000@gmail.com>
2021-03-24 19:53:20 +0100

    Changed SplashActivity

--- app/src/main/java/com/example/mytrainingschedules/activities/appintro/SplashActivity.java
+++ app/src/main/java/com/example/mytrainingschedules/activities/appintro/SplashActivity.java
@@ -38,11 +38,8 @@ import java.util.Map;
 public class SplashActivity extends AppCompatActivity {
 
     private final int SPLASH_DISPLAY_LENGTH = 2000;
-    boolean firstAccess = false, accessToServer = true;
-    String name;
-    String email;
-    TextView errorTextView;
-    ProgressBar progressBar;
+    boolean firstAccess;
+    String guid;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -50,17 +47,10 @@ public class SplashActivity extends AppCompatActivity {
         setContentView(R.layout.splash_layout);
 
         firstAccess = false;
-        accessToServer = true;
-        name = "";
-        email = "";
-        errorTextView = findViewById(R.id.errorTextView);
-        errorTextView.setText("");
-        progressBar = findViewById(R.id.progressBar);
-        progressBar.setVisibility(View.VISIBLE);
 
         if(fileExists(getApplicationContext(), "guid")){
-            /* file "guid" is present, read the content and get user info */
-            String guid = "";
+            /* file "guid" is present, read the content and go to the MainActivity */
+            guid = "";
             try {
                 FileInputStream fileInputStream = getApplicationContext().openFileInput("guid");
                 InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream);
@@ -72,37 +62,26 @@ public class SplashActivity extends AppCompatActivity {
                 e.printStackTrace();
             }
             Log.d("APP_DEBUG", "GUID: " + guid);
-
-            /* POST */
-            String url = "http://192.168.0.109:8080/userinfo";
-            JSONObject jsonObject = new JSONObject();
-            try {
-                jsonObject.put("guid", guid);
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-            getUserInfo(getApplicationContext(), url, jsonObject);
         }
         else{
+            /* file "guid" doesn't exists, go to the IntroActivity */
             Log.d("APP_DEBUG", "guid file doesn't exists, going to IntroActivity");
             firstAccess = true;
         }
 
-        /* New Handler to start the LoginActivity
-         * and close this Splash-Screen after some seconds.*/
         new Handler().postDelayed(new Runnable(){
             @Override
             public void run() {
+                Intent intent;
                 if(firstAccess){
-                    Intent intent = new Intent(SplashActivity.this, IntroActivity.class);
-                    SplashActivity.this.startActivity(intent);
-                    SplashActivity.this.finish();
+                    intent = new Intent(SplashActivity.this, IntroActivity.class);
                 }
-                else if(!firstAccess && accessToServer){
-                    Intent intent = new Intent(SplashActivity.this, MainActivity.class);
-                    startActivity(intent);
-                    SplashActivity.this.finish();
+                else{
+                    intent = new Intent(SplashActivity.this, MainActivity.class);
+                    intent.putExtra("USER_GUID", guid);
                 }
+                SplashActivity.this.startActivity(intent);
+                SplashActivity.this.finish();
             }
         }, SPLASH_DISPLAY_LENGTH);
 
@@ -114,74 +93,6 @@ public class SplashActivity extends AppCompatActivity {
             return false;
         }
         return true;
-    }
-
-    public void setName(String name){
-        this.name = name;
-    }
-
-    public void setEmail(String email){
-        this.email = email;
-    }
-
-    public void setAccessToServer(boolean access){
-        this.accessToServer = access;
-    }
-
-    private void getUserInfo(Context context, String url, JSONObject jsonObject) {
-        RequestQueue queue = Volley.newRequestQueue(context);
-        StringRequest sr = new StringRequest(Request.Method.POST,url, new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                Log.d("APP_DEBUG", "Success: " + response.toString());
-                try {
-                    JSONObject jsonResponse = new JSONObject(response.toString());
-                    JSONObject result = jsonResponse.getJSONObject("result");
-                    setName(result.getString("name"));
-                    setEmail(result.getString("email"));
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                progressBar.setVisibility(View.GONE);
-                Log.d("APP_DEBUG", "Fail: " + error.toString());
-                setAccessToServer(false);
-                if(error.toString().equals("com.android.volley.TimeoutError")) {
-                    errorTextView.setText("Can't connect to the server");
-                }
-                else if(error.toString().equals("com.android.volley.AuthFailureError")){
-                    errorTextView.setText("Invalid credentials");
-                }
-                else{
-                    errorTextView.setText("No Internet connection");
-                }
-            }
-        }){
-            @Override
-            protected Map<String,String> getParams(){
-                Map<String,String> params = new HashMap<String, String>();
-                for (Iterator<String> it = jsonObject.keys(); it.hasNext(); ) {
-                    String key = it.next();
-                    try {
-                        params.put(key, jsonObject.getString(key));
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
-                }
-                return params;
-            }
-
-            @Override
-            public Map<String, String> getHeaders() throws AuthFailureError {
-                Map<String,String> params = new HashMap<String, String>();
-                params.put("Content-Type","application/x-www-form-urlencoded");
-                return params;
-            }
-        };
-        queue.add(sr);
     }
 
 }
--- app/src/main/java/com/example/mytrainingschedules/activities/mainactivity/home/HomeFragment.java
+++ app/src/main/java/com/example/mytrainingschedules/activities/mainactivity/home/HomeFragment.java
@@ -1,10 +1,15 @@
 package com.example.mytrainingschedules.activities.mainactivity.home;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,9 +17,25 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.mytrainingschedules.R;
+import com.example.mytrainingschedules.activities.applogin.LoginActivity;
 import com.example.mytrainingschedules.activities.mainactivity.CustomAdapter;
+import com.example.mytrainingschedules.activities.mainactivity.MainActivity;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 public class HomeFragment extends Fragment {
 
@@ -22,6 +43,8 @@ public class HomeFragment extends Fragment {
     GridView gridView;
     CustomAdapter adapter;
     FloatingActionButton fab;
+    TextView errorTextView;
+    ImageView badImageView;
 
     /*
     * getActivity() --> MainActivity
@@ -32,9 +55,25 @@ public class HomeFragment extends Fragment {
         homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.home_fragment, container, false);
 
+        errorTextView = root.findViewById(R.id.errorTextView);
+        errorTextView.setText("");
+        errorTextView.setVisibility(View.GONE);
+        badImageView = root.findViewById(R.id.badImageView);
+        badImageView.setVisibility(View.GONE);
+
+        JSONObject jsonObject = new JSONObject();
+        Log.d("APP_DEBUG", "GUID in Home: " + getActivity().getIntent().getStringExtra("USER_GUID"));
+        try {
+            jsonObject.put("guid", getActivity().getIntent().getStringExtra("USER_GUID"));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        getUserSchedules(getContext(), getResources().getString(R.string.base_url) + "/homeinfo", jsonObject);
+
         String[] data = {"uno", "due", "tre", "quattro", "cinque", "sei", "sette", "otto", "nove"};
         adapter = new CustomAdapter(this.getContext(), data);
 
+        /* maybe this "homeViewModel" is completely useless, for the moment we keep it */
         homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
@@ -44,6 +83,55 @@ public class HomeFragment extends Fragment {
                 gridView.setAdapter(adapter);
             }
         });
+
         return root;
+    }
+
+    private void getUserSchedules(Context context, String url, JSONObject jsonObject) {
+        RequestQueue queue = Volley.newRequestQueue(context);
+        StringRequest sr = new StringRequest(Request.Method.POST,url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.d("APP_DEBUG", "Success: " + response.toString());
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("APP_DEBUG", "Fail: " + error.toString());
+                errorTextView.setVisibility(View.VISIBLE);
+                badImageView.setVisibility(View.VISIBLE);
+                if(error.toString().equals("com.android.volley.TimeoutError")) {
+                    errorTextView.setText("Can't connect to the server");
+                }
+                else if(error.toString().equals("com.android.volley.AuthFailureError")){
+                    errorTextView.setText("Invalid credentials");
+                }
+                else{
+                    errorTextView.setText("No Internet connection");
+                }
+            }
+        }){
+            @Override
+            protected Map<String,String> getParams(){
+                Map<String,String> params = new HashMap<String, String>();
+                for (Iterator<String> it = jsonObject.keys(); it.hasNext(); ) {
+                    String key = it.next();
+                    try {
+                        params.put(key, jsonObject.getString(key));
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+                return params;
+            }
+
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String,String> params = new HashMap<String, String>();
+                params.put("Content-Type","application/x-www-form-urlencoded");
+                return params;
+            }
+        };
+        queue.add(sr);
     }
 }
